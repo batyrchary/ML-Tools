7b6b28bbe39cfde4a663624e20b15277aea94cfe
batyrchary <batyrchary95@gmail.com>
2024-02-09 21:24:26 -0800

    Tensorflow

--- 4_Tensorflow/TensorflowCore_2_7.py
+++ 4_Tensorflow/TensorflowCore_2_7.py
@@ -3,3 +3,108 @@
 #Save and load models 
 #https://www.tensorflow.org/tutorials/keras/keras_tuner
 
+#we can use The Keras Tuner
+#pip install -q -U keras-tuner
+
+
+#Model hyperparameters which influence model selection such as the number and width of hidden layers
+#Algorithm hyperparameters which influence the speed and quality of the learning algorithm such as the learning rate for Stochastic Gradient Descent (SGD) and the number of nearest neighbors for a k Nearest Neighbors (KNN) classifier
+
+
+import tensorflow as tf
+from tensorflow import keras
+import keras_tuner as kt
+
+
+
+(img_train, label_train), (img_test, label_test) = keras.datasets.fashion_mnist.load_data()
+# Normalize pixel values between 0 and 1
+img_train = img_train.astype('float32') / 255.0
+img_test = img_test.astype('float32') / 255.0
+
+
+#The model you set up for hypertuning is called a hypermodel.
+#By using a model builder function
+#By subclassing the HyperModel class of the Keras Tuner API
+
+#You can also use two pre-defined HyperModel classes - HyperXception and HyperResNet for computer vision applications.
+
+
+def model_builder(hp):
+  model = keras.Sequential()
+  model.add(keras.layers.Flatten(input_shape=(28, 28)))
+
+  # Tune the number of units in the first Dense layer
+  # Choose an optimal value between 32-512
+  hp_units = hp.Int('units', min_value=32, max_value=512, step=32)
+  model.add(keras.layers.Dense(units=hp_units, activation='relu'))
+  model.add(keras.layers.Dense(10))
+
+  # Tune the learning rate for the optimizer
+  # Choose an optimal value from 0.01, 0.001, or 0.0001
+  hp_learning_rate = hp.Choice('learning_rate', values=[1e-2, 1e-3, 1e-4])
+
+  model.compile(optimizer=keras.optimizers.Adam(learning_rate=hp_learning_rate),
+                loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                metrics=['accuracy'])
+
+  return model
+
+
+
+#Instantiate the tuner and perform hypertuning
+#To instantiate the Hyperband tuner, you must specify the hypermodel, the objective to optimize and the maximum number of epochs to train (max_epochs).
+tuner = kt.Hyperband(model_builder,
+                     objective='val_accuracy',
+                     max_epochs=10,
+                     factor=3,
+                     directory='my_dir',
+                     project_name='intro_to_kt')
+
+
+
+'''
+The Hyperband tuning algorithm uses adaptive resource allocation and early-stopping to quickly converge on a high-performing model. This is done using a sports championship style bracket. The algorithm trains a large number of models for a few epochs and carries forward only the top-performing half of models to the next round. Hyperband determines the number of models to train in a bracket by computing 1 + logfactor(max_epochs) and rounding it up to the nearest integer.
+
+Create a callback to stop training early after reaching a certain value for the validation loss.
+'''
+
+stop_early = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=5)
+
+tuner.search(img_train, label_train, epochs=50, validation_split=0.2, callbacks=[stop_early])
+
+# Get the optimal hyperparameters
+best_hps=tuner.get_best_hyperparameters(num_trials=1)[0]
+
+print(f"""
+The hyperparameter search is complete. The optimal number of units in the first densely-connected
+layer is {best_hps.get('units')} and the optimal learning rate for the optimizer
+is {best_hps.get('learning_rate')}.
+""")
+
+
+#Train the model
+#Find the optimal number of epochs to train the model with the hyperparameters obtained from the search.
+# Build the model with the optimal hyperparameters and train it on the data for 50 epochs
+model = tuner.hypermodel.build(best_hps)
+history = model.fit(img_train, label_train, epochs=50, validation_split=0.2)
+
+val_acc_per_epoch = history.history['val_accuracy']
+best_epoch = val_acc_per_epoch.index(max(val_acc_per_epoch)) + 1
+print('Best epoch: %d' % (best_epoch,))
+
+
+hypermodel = tuner.hypermodel.build(best_hps)
+
+# Retrain the model
+hypermodel.fit(img_train, label_train, epochs=best_epoch, validation_split=0.2)
+
+
+eval_result = hypermodel.evaluate(img_test, label_test)
+print("[test loss, test accuracy]:", eval_result)
+
+
+#The my_dir/intro_to_kt directory contains detailed logs and checkpoints for 
+#every trial (model configuration) run during the hyperparameter search. 
+#If you re-run the hyperparameter search, the Keras Tuner uses the existing state from these logs to resume the search. To disable this behavior, pass an additional overwrite=True argument while instantiating the tuner.
+
---
+++ 4_Tensorflow/TensorflowCore_2_9.py
@@ -0,0 +1,454 @@
+#Load and preprocess data
+
+#videos
+#https://www.tensorflow.org/tutorials/load_data/video
+
+
+'''
+You will learn how to:
+
+Load the data from a zip file.
+
+Read sequences of frames out of the video files.
+
+Visualize the video data.
+
+Wrap the frame-generator tf.data.Dataset.
+
+Setup
+Begin by installing and importing some necessary libraries, including: remotezip to inspect the contents of a ZIP file, tqdm to use a progress bar, OpenCV to process video files, and tensorflow_docs for embedding data in a Jupyter notebook.
+'''
+
+# The way this tutorial uses the `TimeDistributed` layer requires TF>=2.10
+#pip install -U "tensorflow>=2.10.0"
+
+#pip install remotezip tqdm opencv-python
+#pip install -q git+https://github.com/tensorflow/docs
+
+import tqdm
+import random
+import pathlib
+import itertools
+import collections
+
+import os
+import cv2
+import numpy as np
+import remotezip as rz
+
+import tensorflow as tf
+
+# Some modules to display an animation using imageio.
+import imageio
+from IPython import display
+from urllib import request
+from tensorflow_docs.vis import embed
+
+
+#Download a subset of the UCF101 dataset
+URL = 'https://storage.googleapis.com/thumos14_files/UCF101_videos.zip'
+
+
+def list_files_from_zip_url(zip_url):
+  """ List the files in each class of the dataset given a URL with the zip file.
+
+    Args:
+      zip_url: A URL from which the files can be extracted from.
+
+    Returns:
+      List of files in each of the classes.
+  """
+  files = []
+  with rz.RemoteZip(zip_url) as zip:
+    for zip_info in zip.infolist():
+      files.append(zip_info.filename)
+  return files
+
+files = list_files_from_zip_url(URL)
+files = [f for f in files if f.endswith('.avi')]
+files[:10]
+
+
+#Begin with a few videos and a limited number of classes for training. After running the above code block, notice that the class name is included in the filename of each video.
+
+#Define the get_class function that retrieves the class name from a filename. Then, create a function called get_files_per_class which converts the list of all files (files above) into a dictionary listing the files for each class:
+
+def get_class(fname):
+  """ Retrieve the name of the class given a filename.
+
+    Args:
+      fname: Name of the file in the UCF101 dataset.
+
+    Returns:
+      Class that the file belongs to.
+  """
+  return fname.split('_')[-3]
+
+
+
+def get_files_per_class(files):
+  """ Retrieve the files that belong to each class.
+
+    Args:
+      files: List of files in the dataset.
+
+    Returns:
+      Dictionary of class names (key) and files (values). 
+  """
+  files_for_class = collections.defaultdict(list)
+  for fname in files:
+    class_name = get_class(fname)
+    files_for_class[class_name].append(fname)
+  return files_for_class
+
+
+#Once you have the list of files per class, you can choose how many classes you would like to use and how many videos you would like per class in order to create your dataset.  
+
+NUM_CLASSES = 10
+FILES_PER_CLASS = 50
+
+files_for_class = get_files_per_class(files)
+classes = list(files_for_class.keys())
+
+print('Num classes:', len(classes))
+print('Num videos for class[0]:', len(files_for_class[classes[0]]))
+
+
+#Create a new function called select_subset_of_classes that selects a subset of the classes present within the dataset and a particular number of files per class:
+
+def select_subset_of_classes(files_for_class, classes, files_per_class):
+  """ Create a dictionary with the class name and a subset of the files in that class.
+
+    Args:
+      files_for_class: Dictionary of class names (key) and files (values).
+      classes: List of classes.
+      files_per_class: Number of files per class of interest.
+
+    Returns:
+      Dictionary with class as key and list of specified number of video files in that class.
+  """
+  files_subset = dict()
+
+  for class_name in classes:
+    class_files = files_for_class[class_name]
+    files_subset[class_name] = class_files[:files_per_class]
+
+  return files_subset
+
+
+files_subset = select_subset_of_classes(files_for_class, classes[:NUM_CLASSES], FILES_PER_CLASS)
+list(files_subset.keys())
+
+
+#Define helper functions that split the videos into training, validation, and test sets. The videos are downloaded from a URL with the zip file, and placed into their respective subdirectiories.
+
+def download_from_zip(zip_url, to_dir, file_names):
+  """ Download the contents of the zip file from the zip URL.
+
+    Args:
+      zip_url: A URL with a zip file containing data.
+      to_dir: A directory to download data to.
+      file_names: Names of files to download.
+  """
+  with rz.RemoteZip(zip_url) as zip:
+    for fn in tqdm.tqdm(file_names):
+      class_name = get_class(fn)
+      zip.extract(fn, str(to_dir / class_name))
+      unzipped_file = to_dir / class_name / fn
+
+      fn = pathlib.Path(fn).parts[-1]
+      output_file = to_dir / class_name / fn
+      unzipped_file.rename(output_file)
+
+
+#The following function returns the remaining data that hasn't already been placed into a subset of data. It allows you to place that remaining data in the next specified subset of data.
+
+def split_class_lists(files_for_class, count):
+  """ Returns the list of files belonging to a subset of data as well as the remainder of
+    files that need to be downloaded.
+
+    Args:
+      files_for_class: Files belonging to a particular class of data.
+      count: Number of files to download.
+
+    Returns:
+      Files belonging to the subset of data and dictionary of the remainder of files that need to be downloaded.
+  """
+  split_files = []
+  remainder = {}
+  for cls in files_for_class:
+    split_files.extend(files_for_class[cls][:count])
+    remainder[cls] = files_for_class[cls][count:]
+  return split_files, remainder
+
+
+
+#The following download_ucf_101_subset function allows you to download a subset of the UCF101 dataset and split it into the training, validation, and test sets. You can specify the number of classes that you would like to use. The splits argument allows you to pass in a dictionary in which the key values are the name of subset (example: "train") and the number of videos you would like to have per class.
+
+def download_ucf_101_subset(zip_url, num_classes, splits, download_dir):
+  """ Download a subset of the UCF101 dataset and split them into various parts, such as
+    training, validation, and test.
+
+    Args:
+      zip_url: A URL with a ZIP file with the data.
+      num_classes: Number of labels.
+      splits: Dictionary specifying the training, validation, test, etc. (key) division of data 
+              (value is number of files per split).
+      download_dir: Directory to download data to.
+
+    Return:
+      Mapping of the directories containing the subsections of data.
+  """
+  files = list_files_from_zip_url(zip_url)
+  for f in files:
+    path = os.path.normpath(f)
+    tokens = path.split(os.sep)
+    if len(tokens) <= 2:
+      files.remove(f) # Remove that item from the list if it does not have a filename
+
+  files_for_class = get_files_per_class(files)
+
+  classes = list(files_for_class.keys())[:num_classes]
+
+  for cls in classes:
+    random.shuffle(files_for_class[cls])
+
+  # Only use the number of classes you want in the dictionary
+  files_for_class = {x: files_for_class[x] for x in classes}
+
+  dirs = {}
+  for split_name, split_count in splits.items():
+    print(split_name, ":")
+    split_dir = download_dir / split_name
+    split_files, files_for_class = split_class_lists(files_for_class, split_count)
+    download_from_zip(zip_url, split_dir, split_files)
+    dirs[split_name] = split_dir
+
+  return dirs
+
+download_dir = pathlib.Path('./UCF101_subset/')
+subset_paths = download_ucf_101_subset(URL,
+                                       num_classes = NUM_CLASSES,
+                                       splits = {"train": 30, "val": 10, "test": 10},
+                                       download_dir = download_dir)
+
+
+#After downloading the data, you should now have a copy of a subset of the UCF101 dataset. Run the following code to print the total number of videos you have amongst all your subsets of data.
+
+video_count_train = len(list(download_dir.glob('train/*/*.avi')))
+video_count_val = len(list(download_dir.glob('val/*/*.avi')))
+video_count_test = len(list(download_dir.glob('test/*/*.avi')))
+video_total = video_count_train + video_count_val + video_count_test
+print(f"Total videos: {video_total}")
+
+#You can also preview the directory of data files now.
+
+#find ./UCF101_subset
+
+
+'''
+Create frames from each video file
+The frames_from_video_file function splits the videos into frames, reads a randomly chosen span of n_frames out of a video file, and returns them as a NumPy array. To reduce memory and computation overhead, choose a small number of frames. In addition, pick the same number of frames from each video, which makes it easier to work on batches of data.
+'''
+
+def format_frames(frame, output_size):
+  """
+    Pad and resize an image from a video.
+
+    Args:
+      frame: Image that needs to resized and padded. 
+      output_size: Pixel size of the output frame image.
+
+    Return:
+      Formatted frame with padding of specified output size.
+  """
+  frame = tf.image.convert_image_dtype(frame, tf.float32)
+  frame = tf.image.resize_with_pad(frame, *output_size)
+  return frame
+
+def frames_from_video_file(video_path, n_frames, output_size = (224,224), frame_step = 15):
+  """
+    Creates frames from each video file present for each category.
+
+    Args:
+      video_path: File path to the video.
+      n_frames: Number of frames to be created per video file.
+      output_size: Pixel size of the output frame image.
+
+    Return:
+      An NumPy array of frames in the shape of (n_frames, height, width, channels).
+  """
+  # Read each video frame by frame
+  result = []
+  src = cv2.VideoCapture(str(video_path))  
+
+  video_length = src.get(cv2.CAP_PROP_FRAME_COUNT)
+
+  need_length = 1 + (n_frames - 1) * frame_step
+
+  if need_length > video_length:
+    start = 0
+  else:
+    max_start = video_length - need_length
+    start = random.randint(0, max_start + 1)
+
+  src.set(cv2.CAP_PROP_POS_FRAMES, start)
+  # ret is a boolean indicating whether read was successful, frame is the image itself
+  ret, frame = src.read()
+  result.append(format_frames(frame, output_size))
+
+  for _ in range(n_frames - 1):
+    for _ in range(frame_step):
+      ret, frame = src.read()
+    if ret:
+      frame = format_frames(frame, output_size)
+      result.append(frame)
+    else:
+      result.append(np.zeros_like(result[0]))
+  src.release()
+  result = np.array(result)[..., [2, 1, 0]]
+
+  return result
+
+
+#Visualize video data
+#curl -O https://upload.wikimedia.org/wikipedia/commons/8/86/End_of_a_jam.ogv
+
+#video_path = "End_of_a_jam.ogv"
+#sample_video = frames_from_video_file(video_path, n_frames = 10)
+#sample_video.shape
+
+def to_gif(images):
+  converted_images = np.clip(images * 255, 0, 255).astype(np.uint8)
+  imageio.mimsave('./animation.gif', converted_images, fps=10)
+  return embed.embed_file('./animation.gif')
+
+to_gif(sample_video)
+
+
+
+# docs-infra: no-execute
+ucf_sample_video = frames_from_video_file(next(subset_paths['train'].glob('*/*.avi')), 50)
+to_gif(ucf_sample_video)
+
+
+#Next, define the FrameGenerator class in order to create an iterable object that can feed data into the TensorFlow data pipeline. The generator (__call__) function yields the frame array produced by frames_from_video_file and a one-hot encoded vector of the label associated with the set of frames.
+
+class FrameGenerator:
+  def __init__(self, path, n_frames, training = False):
+    """ Returns a set of frames with their associated label. 
+
+      Args:
+        path: Video file paths.
+        n_frames: Number of frames. 
+        training: Boolean to determine if training dataset is being created.
+    """
+    self.path = path
+    self.n_frames = n_frames
+    self.training = training
+    self.class_names = sorted(set(p.name for p in self.path.iterdir() if p.is_dir()))
+    self.class_ids_for_name = dict((name, idx) for idx, name in enumerate(self.class_names))
+
+  def get_files_and_class_names(self):
+    video_paths = list(self.path.glob('*/*.avi'))
+    classes = [p.parent.name for p in video_paths] 
+    return video_paths, classes
+
+  def __call__(self):
+    video_paths, classes = self.get_files_and_class_names()
+
+    pairs = list(zip(video_paths, classes))
+
+    if self.training:
+      random.shuffle(pairs)
+
+    for path, name in pairs:
+      video_frames = frames_from_video_file(path, self.n_frames) 
+      label = self.class_ids_for_name[name] # Encode labels
+      yield video_frames, label
+
+
+#Test out the FrameGenerator object before wrapping it as a TensorFlow Dataset object. Moreover, for the training dataset, ensure you enable training mode so that the data will be shuffled.
+fg = FrameGenerator(subset_paths['train'], 10, training=True)
+
+frames, label = next(fg())
+
+print(f"Shape: {frames.shape}")
+print(f"Label: {label}")
+
+
+# Create the training set
+output_signature = (tf.TensorSpec(shape = (None, None, None, 3), dtype = tf.float32),
+                    tf.TensorSpec(shape = (), dtype = tf.int16))
+train_ds = tf.data.Dataset.from_generator(FrameGenerator(subset_paths['train'], 10, training=True),
+                                          output_signature = output_signature)
+
+
+for frames, labels in train_ds.take(10):
+  print(labels)
+
+
+# Create the validation set
+val_ds = tf.data.Dataset.from_generator(FrameGenerator(subset_paths['val'], 10),
+                                        output_signature = output_signature)
+
+
+
+
+# Print the shapes of the data
+train_frames, train_labels = next(iter(train_ds))
+print(f'Shape of training set of frames: {train_frames.shape}')
+print(f'Shape of training labels: {train_labels.shape}')
+
+val_frames, val_labels = next(iter(val_ds))
+print(f'Shape of validation set of frames: {val_frames.shape}')
+print(f'Shape of validation labels: {val_labels.shape}')
+
+
+#Configure the dataset for performance
+'''
+Use buffered prefetching such that you can yield data from the disk without having I/O become blocking. Two important functions to use while loading data are:
+
+Dataset.cache: keeps the sets of frames in memory after they're loaded off the disk during the first epoch. This function ensures that the dataset does not become a bottleneck while training your model. If your dataset is too large to fit into memory, you can also use this method to create a performant on-disk cache.
+
+Dataset.prefetch: overlaps data preprocessing and model execution while training. Refer to Better performance with the tf.data for details.
+
+
+AUTOTUNE = tf.data.AUTOTUNE
+
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size = AUTOTUNE)
+val_ds = val_ds.cache().shuffle(1000).prefetch(buffer_size = AUTOTUNE)
+To prepare the data to be fed into the model, use batching as shown below. Notice that when working with video data, such as AVI files, the data should be shaped as a five dimensional object. These dimensions are as follows: [batch_size, number_of_frames, height, width, channels]. In comparison, an image would have four dimensions: [batch_size, height, width, channels]. The image below is an illustration of how the shape of video data is represented.
+'''
+
+train_ds = train_ds.batch(2)
+val_ds = val_ds.batch(2)
+
+train_frames, train_labels = next(iter(train_ds))
+print(f'Shape of training set of frames: {train_frames.shape}')
+print(f'Shape of training labels: {train_labels.shape}')
+
+val_frames, val_labels = next(iter(val_ds))
+print(f'Shape of validation set of frames: {val_frames.shape}')
+print(f'Shape of validation labels: {val_labels.shape}')
+
+#Now that you have created a TensorFlow Dataset of video frames with their labels, you can use it with a deep learning model. The following classification model that uses a pre-trained EfficientNet trains to high accuracy in a few minutes:
+
+net = tf.keras.applications.EfficientNetB0(include_top = False)
+net.trainable = False
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Rescaling(scale=255),
+    tf.keras.layers.TimeDistributed(net),
+    tf.keras.layers.Dense(10),
+    tf.keras.layers.GlobalAveragePooling3D()
+])
+
+model.compile(optimizer = 'adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits = True),
+              metrics=['accuracy'])
+
+model.fit(train_ds, 
+          epochs = 10,
+          validation_data = val_ds,
+          callbacks = tf.keras.callbacks.EarlyStopping(patience = 2, monitor = 'val_loss'))
